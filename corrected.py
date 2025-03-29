2ecf337ffe32ddb7922c17d55889a0c4521cc066
Ojas Kataria <63237038+IrishBumfuzzle@users.noreply.github.com>
2025-03-29 10:16:12 +0530

    Added correction comments

--- corrected.py
+++ corrected.py
@@ -1,17 +1,17 @@
 def find_cube_pairs(target):
     solutions = []
-    max_num = round(target ** (1/3))  
+    max_num = round(target ** (1/3))  # double star instead of 3
 
-    for a in range(1, max_num + 1):
-        for b in range(a, max_num + 1):
-            if a**3 + b**3 == target:
-                solutions.append((a, b))
-    return solutions
+    for a in range(1, max_num + 1): # add colon 
+        for b in range(a, max_num + 1): # add colon
+            if a**3 + b**3 == target: # two stars instead of three
+                solutions.append((a, b)) # solutions instead if sol
+    return solutions # solution instead of sol
 
-pairs = find_cube_pairs(1729)
-print("Valid cube pairs for 1729:")
-for a, b in pairs:
-    print(f" → {a}³ + {b}³ = {a**3} + {b**3} = 1729")
+pairs = find_cube_pairs(1729) # remove comma
+print("Valid cube pairs for 1729:") # 1729 instead of 1728 and remove comma
+for a, b in pairs: # add colon 
+    print(f" → {a}³ + {b}³ = {a**3} + {b**3} = 1729") # **3 instead of **2
 """Submit your response here:  https://forms.office.com/Pages/ResponsePage.aspx?id=vDsaA3zPK06W7IZ1VVQKHFzW4INMf2JMjyL9qPnlPbNUMFU2TjI1WjQyUlczSFNIOFBEWkxTQ0lFQS4u"""
 
 """
